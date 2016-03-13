34836bcf4156b661df0d3ea604199cfd2e4d45a3
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-03-13 19:48:14 +0200

    Routing and templates set up

---
+++ gruntfile.js
@@ -0,0 +1,50 @@
+'use strict';
+var path = require('path');
+
+module.exports = function(grunt) {
+	// Do grunt-related things in here
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded'
+				},
+				files: {
+					'css/main.css': 'sass/main.scss'
+				}
+			}
+		},
+	    watch: {
+	    	options: { livereload: true },
+	      	sass: {
+	        	files: ['sass/main.scss'],
+	        	tasks: ['newer:sass:dist'],
+	      	},
+	      	html: {
+	      		files: ['index.html', '**/*.html', '**/*.js' ],
+	      	}
+	    },
+	    bumpup: {
+	        file: 'package.json'
+	    },
+		connect: {
+			server: {
+				options: {
+					port: 9005,
+					base: '',
+					hostname: '*',
+					livereload:true
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-newer');
+	grunt.loadNpmTasks('grunt-bumpup');
+	grunt.task.registerTask('default', ['connect','watch']);
+
+};
